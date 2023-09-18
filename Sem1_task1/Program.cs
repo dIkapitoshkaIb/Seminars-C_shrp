bf16dbfdc7c85059132a1902f3d88c2dd5a8c594
dIkapitoshkaIb <burlingis.k@gmail.com>
2023-09-18 15:15:44 +0300

    возведение в квадрат

---
+++ Sem1_task1/Program.cs
@@ -0,0 +1,5 @@
+﻿// написать программу, которая на вход принимает число и умножает его на себя(возводит в квадрат)
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine(number * number);
