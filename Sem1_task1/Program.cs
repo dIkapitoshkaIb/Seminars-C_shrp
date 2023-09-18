f85d9e75c4593d93dea3046f1352d258b5c54d1f
dIkapitoshkaIb <burlingis.k@gmail.com>
2023-09-18 17:28:11 +0300

    добавил код определения дня недели

--- Sem1_task1/Program.cs
+++ Sem1_task1/Program.cs
@@ -1,5 +1,55 @@
-﻿// написать программу, которая на вход принимает число и умножает его на себя(возводит в квадрат)
+﻿/* // написать программу, которая на вход принимает число и умножает его на себя(возводит в квадрат)
 
 Console.WriteLine("Введите число: ");
 int number = int.Parse(Console.ReadLine()!);
-Console.WriteLine(number * number);
+Console.WriteLine(number * number); */
+
+/* //Написать программу для нахождения квадрата.
+
+Console.WriteLine("Введите число a: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число b: ");
+int b = int.Parse(Console.ReadLine()!);
+
+if (a == (b * b))
+{
+    Console.WriteLine("Является квадратом");
+}
+else
+{
+    Console.WriteLine("Не является квадратом");
+} */
+
+// Написать программу, определяющую выходной день в неделе.
+
+using System.Diagnostics;
+
+Console.WriteLine("Введите день недели: ");
+string num = Console.ReadLine();
+switch(num)
+{
+    case "1":
+    Console.WriteLine("Пн");
+    break;
+    case "2":
+    Console.WriteLine("Вт");
+    break;
+    case "3":
+    Console.WriteLine("Ср");
+    break;
+    case "4":
+    Console.WriteLine("Чт");
+    break;
+    case "5":
+    Console.WriteLine("Пт");
+    break;
+    case "6":
+    Console.WriteLine("Сб");
+    break;
+    case "7":
+    Console.WriteLine("Вс");
+    break;
+    default:
+    Console.WriteLine("Нет такого дня недели");
+    break;
+}
